56e12fa181e100653e5c15865fc03276992894e4
IMRANDIL <aliimranadil2@gmail.com>
2022-11-06 05:59:39 +0530

    healthum

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-
+import { Button, Form, Input } from "antd";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+  const handleFinish = (values) => {
+    console.log("value of form ", values);
+  };
+
+  return (
+    <div className="auth">
+      <div className="authForm card p-4">
+        <h1 className="card_title">Nice to meet u :)</h1>
+        <Form layout="vertical" onFinish={handleFinish}>
+          <Form.Item label="Email" name="email">
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
+          <Button className="primaryButton mt-3" htmlType="submit">
+            Login
+          </Button>
+          <Link to="/signup" className="anchor">
+            Click here to Signup
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
