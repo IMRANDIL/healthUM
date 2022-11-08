ff5c78d7d17d1b8ffa73cba14e583925bf185899
IMRANDIL <aliimranadil2@gmail.com>
2022-11-08 12:27:02 +0530

    yes do it

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,10 +2,49 @@ import React from "react";
 import "./Layout.css";
 
 const Layout = ({ children }) => {
+  const userMenu = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "",
+    },
+    {
+      name: "Appointments",
+      path: "/appointments",
+      icon: "",
+    },
+    {
+      name: "Apply Doctor",
+      path: "/apply-doctor",
+      icon: "",
+    },
+    {
+      name: "Profile",
+      path: "/profile",
+      icon: "",
+    },
+    {
+      name: "Logout",
+      path: "/logout",
+      icon: "",
+    },
+  ];
+
+  const menuToBeRendered = userMenu;
+
   return (
     <div className="main">
       <div className="d-flex layout">
-        <div className="sidebar">Sidebar</div>
+        <div className="sidebar">
+          <div className="sidebar-header">
+            <h1>HTHM</h1>
+          </div>
+          <div className="menu">
+            {menuToBeRendered.map((menu) => {
+              return <div></div>;
+            })}
+          </div>
+        </div>
         <div className="content">
           <div className="header">Header</div>
           <div className="body">{children}</div>
