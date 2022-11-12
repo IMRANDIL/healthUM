b0e1a2d5689e4bd4b187d5dc087052183c94ddcd
IMRANDIL <aliimranadil2@gmail.com>
2022-11-12 16:24:55 +0530

    yes some modal changes

--- models/userModel.js
+++ models/userModel.js
@@ -17,6 +17,22 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    seenNotifications: {
+      type: Array,
+      default: [],
+    },
+    unseenNotifications: {
+      type: Array,
+      default: [],
+    },
   },
   {
     timestamps: true,
