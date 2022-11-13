bb75394e9f5bf885f6a610348d33744c00c4c4fd
IMRANDIL <aliimranadil2@gmail.com>
2022-11-13 08:12:59 +0530

    doctor model done

---
+++ models/doctorModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const doctorSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    specialization: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+    },
+    feePerConsultaion: {
+      type: Number,
+      required: true,
+    },
+    fromTime: {
+      type: String,
+      required: true,
+    },
+    toTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Doctor", doctorSchema);
