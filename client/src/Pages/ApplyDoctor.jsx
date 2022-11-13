516c799138a0e276177dc558fa75c7fd152857be
IMRANDIL <aliimranadil2@gmail.com>
2022-11-13 10:31:10 +0530

    some more layout change

--- client/src/Pages/ApplyDoctor.jsx
+++ client/src/Pages/ApplyDoctor.jsx
@@ -5,8 +5,10 @@ const ApplyDoctor = () => {
   return (
     <Layout>
       <h1 className="page-title">Apply Doctor</h1>
+      <hr />
       <Form layout="vertical">
-        <Row>
+        <h1 className="card_title">Personal Info :)</h1>
+        <Row gutter={20}>
           <Col span={8} xs={24} sm={24} lg={8}>
             <Form.Item
               required
@@ -56,6 +58,61 @@ const ApplyDoctor = () => {
               rules={[{ required: true }]}
             >
               <Input placeholder="Address" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <hr />
+        <h1 className="card_title">Professional Info :)</h1>
+        <Row gutter={20}>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Specialization"
+              name="specialization"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Specialization" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Experience"
+              name="experience"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Experience" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="Consultaion Fee"
+              name="feePerConsultation"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="Consultation Fee" />
+            </Form.Item>
+          </Col>
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="From Time"
+              name="fromTime"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="From Time" />
+            </Form.Item>
+          </Col>
+
+          <Col span={8} xs={24} sm={24} lg={8}>
+            <Form.Item
+              required
+              label="To Time"
+              name="toTime"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="To Time" />
             </Form.Item>
           </Col>
         </Row>
