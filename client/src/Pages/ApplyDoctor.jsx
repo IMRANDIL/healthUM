15196d1bdbc3672298a1229b289dd9ee2783524b
IMRANDIL <aliimranadil2@gmail.com>
2022-11-19 15:26:48 +0530

    yes some development

--- client/src/Pages/ApplyDoctor.jsx
+++ client/src/Pages/ApplyDoctor.jsx
@@ -1,9 +1,55 @@
 import React from "react";
 import Layout from "../components/Layout";
 import { Button, Row, Col, Form, Input, TimePicker } from "antd";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { hideLoading, showLoading } from "../Redux/alertsSlice";
 const ApplyDoctor = () => {
-  const handleFinish = (values) => {
-    console.log("success ", values);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.user);
+  const handleFinish = async (values) => {
+    if (
+      !values.firstName ||
+      !values.lastName ||
+      !values.mobileNumber ||
+      !values.website ||
+      !values.address ||
+      !values.specialization ||
+      !values.experience ||
+      !values.feePerConsultation ||
+      !values.timings
+    ) {
+      return toast.error("All Fields required!", {
+        duration: 1000,
+      });
+    }
+    try {
+      dispatch(showLoading());
+      const response = await axios.post("/api/user/apply-doctor-account", {
+        ...values,
+        userId: user._id,
+      });
+      dispatch(hideLoading());
+      if (response && response.data.success) {
+        toast.success(response.data.msg);
+        return navigate("/");
+      } else {
+        return toast.error(response.data.msg, {
+          duration: 1000,
+        });
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      toast.error(
+        error.response.data.msg ? error.response.data.msg : error.message,
+        {
+          duration: 1000,
+        }
+      );
+    }
   };
 
   return (
