1ef9137b99c8452532b2931a4271bf0103b29ebc
IMRANDIL <aliimranadil2@gmail.com>
2022-11-20 16:51:54 +0530

    yes some more notifications

--- client/src/Pages/Notification.jsx
+++ client/src/Pages/Notification.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import Layout from '../components/Layout';
-import {Tabs} from 'antd';
+import {notification, Tabs} from 'antd';
+import { useSelector,useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 
 const Notification = () => {
-    const items = [
-        { label: 'Unseen', key: 'item-1', children: 'Unseen' },
-        { label: 'Seen', key: 'item-2', children: 'Seen' },
-      ];
 
+  const {user} = useSelector(state=>state.user)
+const navigate = useNavigate()
   return (
    <Layout>
     <h1 className="page-title">Notifications</h1>
-    <Tabs items={items} />
+    <Tabs>
+      <Tabs.TabPane tab='Unseen' key={0}>
+<div className="d-flex justify-content-end">
+  <h1 className="anchor">Mark all as seen</h1>
+</div>
+{user && user.unseenNotifications.map((notification,index)=>(
+  <div className='card p-2' onClick={()=>navigate(notification.onClickPath)} key={index}>
+    <div className="card-text">
+      {notification.msg}
+    </div>
+  </div>
+))}
+      </Tabs.TabPane>
+
+      <Tabs.TabPane tab='Seen' key={1}>
+<div className="d-flex justify-content-end">
+  <h1 className="anchor">Delete all seen</h1>
+</div>
+      </Tabs.TabPane>
+    </Tabs>
    </Layout>
   )
 }
