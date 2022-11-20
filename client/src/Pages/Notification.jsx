92f41c52f08f75a7b90ba4b194df25b9754ebe22
IMRANDIL <aliimranadil2@gmail.com>
2022-11-20 09:34:12 +0530

    added notificatinos tab

--- client/src/Pages/Notification.jsx
+++ client/src/Pages/Notification.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import {Tabs} from 'antd';
+
 
 const Notification = () => {
+    const items = [
+        { label: 'Tab 1', key: 'item-1', children: 'Content 1' },
+        { label: 'Tab 2', key: 'item-2', children: 'Content 2' },
+      ];
+
   return (
    <Layout>
     <h1 className="page-title">Notifications</h1>
+    <Tabs items={items}/>
    </Layout>
   )
 }
