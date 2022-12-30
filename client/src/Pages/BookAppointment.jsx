8fb571f3cb2c3cbfd5a3925effb4d29051c7af41
IMRANDIL <aliimranadil2@gmail.com>
2022-12-30 18:38:38 +0530

    some more push

--- client/src/Pages/BookAppointment.jsx
+++ client/src/Pages/BookAppointment.jsx
@@ -5,9 +5,15 @@ import { useDispatch } from "react-redux";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { hideLoading, showLoading } from '../Redux/alertsSlice'
+import moment from 'moment';
+import { Button, Col, DatePicker, Row, TimePicker } from 'antd';
+
 
 const BookAppointment = () => {
-    const [doctor,setDoctor] = useState(null)
+    const [doctor,setDoctor] = useState(null);
+    const [isAvailable, setIsAvailable] = useState(false);
+    const [date,setDate] = useState();
+    const [selectedTiming, setSelectedTiming] = useState()
     const dispatch = useDispatch();
     const navigate = useNavigate()
     const {doctorId} = useParams()
@@ -36,7 +42,7 @@ const BookAppointment = () => {
             );
             dispatch(hideLoading());
             if (response && response.data.success) {
-             setDoctor(response.data.user)
+             setDoctor(response.data.doctor)
               
             } 
           } catch (error) {
@@ -56,7 +62,29 @@ const BookAppointment = () => {
 
   return (
     <Layout>
-        <div className="page-title">{doctor.firstName} {doctor.lastName}</div>
+        {doctor && (
+            <div>
+ <h1 className="page-title">{doctor.firstName} {doctor.lastName}</h1>
+ <hr />
+<Row>
+    <Col span={8} sm={24} xs={24} lg={8}>
+    <h1 className="normal_text"><b>Timings : </b>{doctor.timings[0]} - {doctor.timings[1]}</h1>
+ <div className="d-flex flex-column pt-2">
+    <DatePicker format='DD-MM-YYYY'
+    onChange={(value)=>setDate(moment(value).format('DD-MM-YYYY'))}
+    />
+    <TimePicker.RangePicker format='HH:mm' className='mt-3'onChange={(value)=>setSelectedTiming(moment(value).format('HH:mm'))}/>
+    <Button
+    type='primary'
+    className='mt-3'
+    >Check Availability</Button>
+ </div>
+    </Col>
+</Row>
+            </div>
+           
+        )}
+        
     </Layout>
   )
 }
