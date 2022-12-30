7c436ad73e9d33a774a56a6b92aa0f5316c62e3f
IMRANDIL <aliimranadil2@gmail.com>
2022-12-30 15:14:19 +0530

    some more components

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,19 +1,55 @@
 import React,{useState,useEffect} from "react";
-// import axios from "axios";
-// import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../Redux/alertsSlice";
+import toast from "react-hot-toast";
+import axios from "axios";
 import Layout from "../components/Layout";
+import Doctor from "../components/Doctor";
+
+
+
+
 const Home = () => {
+  const dispatch = useDispatch()
 const [doctors,setDoctors] = useState([])
-
+const token = localStorage.getItem("token");
 useEffect(()=>{
-
-},[])
+const fetchApprovedDoctors = async()=>{
+  try {
+    dispatch(showLoading());
+    const response = await axios.get(
+      "/api/user/get-all-approved-doctors",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    dispatch(hideLoading());
+    if (response && response.data.success) {
+      return setDoctors(response.data.approvedDoctors);
+    }
+  } catch (error) {
+    dispatch(hideLoading());
+      toast.error(
+        error.response.data.msg ? error.response.data.msg : error.message,
+        {
+          duration: 1000,
+        }
+      );
+  }
+}
+fetchApprovedDoctors()
+},[dispatch,token])
 
 
 
   return (
     <Layout>
       <h1>Home Page</h1>
+      {doctors && doctors.map((doctor)=>(
+        <Doctor key={doctor._id} doctor={doctor}/>
+      ))}
     </Layout>
   );
 };
