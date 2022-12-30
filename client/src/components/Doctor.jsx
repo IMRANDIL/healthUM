1cc71ed9a1f58b2a66549c867729efe2e1be4737
IMRANDIL <aliimranadil2@gmail.com>
2022-12-30 15:39:22 +0530

    some more changes

--- client/src/components/Doctor.jsx
+++ client/src/components/Doctor.jsx
@@ -4,6 +4,7 @@ const Doctor = ({doctor}) => {
   return (
    <div className="card p-2">
     <h1 className="card_title">{doctor.firstName} {doctor.lastName}</h1>
+    <hr/>
     <p className="card_text"><b>Experience : </b>{doctor.experience} Yrs</p>
     <p className="card_text"><b>Mobile Number : </b>{doctor.mobileNumber}</p>
     <p className="card-text"><b>Address : </b>{doctor.address}</p>
