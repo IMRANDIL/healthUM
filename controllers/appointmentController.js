5b50f86e50b1ce7e9ff7bbd732a7a4f611cf9f64
IMRANDIL <aliimranadil2@gmail.com>
2022-12-31 09:19:52 +0530

    some changes

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,4 +1,6 @@
 const Appointment = require("../models/appointmentModel");
+const Doctor = require("../models/doctorModel");
+const User = require("../models/userModel");
 
 class BookAppointment {
   setAppointment = async (req, res, next) => {
@@ -6,6 +8,19 @@ class BookAppointment {
       req.body.status = "pending";
       const newAppointment = new Appointment(req.body);
       await newAppointment.save();
+      const findDoctor = await Doctor.findOne({ _id: req.body.doctorId });
+      if (!findDoctor) {
+        return res.status(404).json({
+          success: false,
+          msg: "Doctor does not exist!",
+        });
+      }
+      findDoctor.unseenNotifications.push({
+        type: "New appointment request",
+        msg: `A new appointment request has been made by ${req.body.user.name}`,
+        onClickPath: "/doctor/appointments",
+      });
+      await findDoctor.save();
     } catch (error) {
       console.log(error);
       res.status(500).send({ msg: "Something went wrong!", success: false });
