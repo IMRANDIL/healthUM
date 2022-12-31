fe34c7aa13c2b85201edf1ddd934719136ca0121
IMRANDIL <aliimranadil2@gmail.com>
2022-12-31 15:59:44 +0530

    one more route added

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,73 +1,70 @@
-const Doctor = require('../models/doctorModel')
-
+const Doctor = require("../models/doctorModel");
+const Appointment = require("../models/appointmentModel");
 
 class DoctorContr {
-getDoctorByuserId = async(req,res,next)=>{
-try {
-    const user = await Doctor.findOne({userId: req.body.userId});
-    if(!user){
-        return res.status(404).json({
-            success:false,
-            msg:'User does not exist!'
-        })
-    }
-
-    res.status(200).json({
-        success:true,
-        msg:'user doctor fetched',
-        user
-
-    })
-
-} catch (error) {
-    res.status(500).send({ msg: "Something went wrong!", success: false });
-}
-}
-
-updateDoctor = async(req,res,next)=>{
+  getDoctorByuserId = async (req, res, next) => {
     try {
-        const user = await Doctor.findOne({userId: req.body.userId});
-        if(!user){
-            return res.status(404).json({
-                success:false,
-                msg:'User does not exist!'
-            })
-        }
-    
-        await Doctor.findByIdAndUpdate(user._id,req.body);
-        res.status(201).json({
-            success:true,
-            msg:'Profile Updated!'
-        })
-    
+      const user = await Doctor.findOne({ userId: req.body.userId });
+      if (!user) {
+        return res.status(404).json({
+          success: false,
+          msg: "User does not exist!",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        msg: "user doctor fetched",
+        user,
+      });
     } catch (error) {
-        console.log(error)
-        res.status(500).send({ msg: "Something went wrong!", success: false });
+      res.status(500).send({ msg: "Something went wrong!", success: false });
     }
-    }
+  };
 
+  updateDoctor = async (req, res, next) => {
+    try {
+      const user = await Doctor.findOne({ userId: req.body.userId });
+      if (!user) {
+        return res.status(404).json({
+          success: false,
+          msg: "User does not exist!",
+        });
+      }
 
-    getDoctorByDoctorId = async(req,res,next)=>{
-        try {
-            const doctor = await Doctor.findOne({_id: req.body.doctorId});
-            if(!doctor){
-                return res.status(404).json({
-                    success:false,
-                    msg:'Doctor does not exist!'
-                })
-            }
-            res.status(200).json({
-                success:true,
-                msg:'doctor details fetched',
-                doctor
-        
-            })
-        } catch (error) {
-            console.log(error)
-        res.status(500).send({ msg: "Something went wrong!", success: false });
-        }
+      await Doctor.findByIdAndUpdate(user._id, req.body);
+      res.status(201).json({
+        success: true,
+        msg: "Profile Updated!",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ msg: "Something went wrong!", success: false });
     }
-};
+  };
+
+  getDoctorByDoctorId = async (req, res, next) => {
+    try {
+      const doctor = await Doctor.findOne({ _id: req.body.doctorId });
+      if (!doctor) {
+        return res.status(404).json({
+          success: false,
+          msg: "Doctor does not exist!",
+        });
+      }
+      res.status(200).json({
+        success: true,
+        msg: "doctor details fetched",
+        doctor,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ msg: "Something went wrong!", success: false });
+    }
+  };
+
+  getAppointmentsInfo = async (req, res, next) => {};
+}
 
 const doctorClass = new DoctorContr();
-module.exports = {doctorClass}
+module.exports = { doctorClass };
--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -1,11 +1,22 @@
-const router = require('express').Router();
-const {doctorClass} = require('../controllers/doctorController')
-const {authMiddleware} = require('../middlewares/authMiddleware')
+const router = require("express").Router();
+const { doctorClass } = require("../controllers/doctorController");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
+router.post(
+  "/get-doctor-info-by-userId",
+  authMiddleware,
+  doctorClass.getDoctorByuserId
+);
+router.post(
+  "/get-doctor-info-by-doctorId",
+  authMiddleware,
+  doctorClass.getDoctorByDoctorId
+);
+router.put("/update-doctor-profile", authMiddleware, doctorClass.updateDoctor);
+router.get(
+  "/get-appointments-by-doctorId",
+  authMiddleware,
+  doctorClass.getAppointmentsInfo
+);
 
-router.post('/get-doctor-info-by-userId',authMiddleware,doctorClass.getDoctorByuserId);
-router.post('/get-doctor-info-by-doctorId',authMiddleware,doctorClass.getDoctorByDoctorId);
-router.put('/update-doctor-profile',authMiddleware,doctorClass.updateDoctor)
-
-
-module.exports = router
+module.exports = router;
