ad275ac48d165c5d8aaa08d3976cd7227cab325d
IMRANDIL <aliimranadil2@gmail.com>
2022-12-31 08:44:36 +0530

    some new changes

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,6 +1,14 @@
 const Appointment = require("../models/appointmentModel");
 
-class BookAppointment {}
+class BookAppointment {
+  setAppointment = async (req, res, next) => {
+    try {
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ msg: "Something went wrong!", success: false });
+    }
+  };
+}
 
 const appointmentClass = new BookAppointment();
 module.exports = { appointmentClass };
--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 const { authMiddleware } = require("../middlewares/authMiddleware");
 const { appointmentClass } = require("../controllers/appointmentController");
 
-router.post("/book-appointment");
+router.post(
+  "/book-appointment",
+  authMiddleware,
+  appointmentClass.setAppointment
+);
 
 module.exports = router;
