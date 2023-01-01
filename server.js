b38b946585f7c3e69c78f6bf936d086322161d9e
IMRANDIL <aliimranadil2@gmail.com>
2023-01-01 13:05:32 +0530

    some more changes

--- server.js
+++ server.js
@@ -11,3 +11,10 @@ app.use("/api/user", require("./routes/authRoute"));
 app.use("/api/admin", require("./routes/adminRoute"));
 app.use("/api/doctor", require("./routes/doctorRoute"));
 app.use("/api/doctor/appointment", require("./routes/appointmentRoute"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.send(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
