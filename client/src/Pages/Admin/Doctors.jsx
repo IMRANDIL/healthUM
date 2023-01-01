29bde648fc7a5040be2b892ee3b277fbd5b081c6
IMRANDIL <aliimranadil2@gmail.com>
2023-01-01 11:31:35 +0530

    some more new year fix

--- client/src/Pages/Admin/Doctors.jsx
+++ client/src/Pages/Admin/Doctors.jsx
@@ -1,162 +1,151 @@
-import React,{useEffect,useState} from 'react';
-import Layout from '../../components/Layout';
-import { setDoctors } from '../../Redux/allDoctorsSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
+import { setDoctors } from "../../Redux/allDoctorsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import axios from "axios";
 import toast from "react-hot-toast";
-import {showLoading,hideLoading} from '../../Redux/alertsSlice'
-import { Table } from 'antd';
-
+import { showLoading, hideLoading } from "../../Redux/alertsSlice";
+import { Table } from "antd";
+import moment from "moment";
 
 const Doctors = () => {
+  const [pages, setPages] = useState(1);
+  const [page, setPage] = useState(1);
+  const [isSuccess, setIsSuccess] = useState(false);
 
+  const dispatch = useDispatch();
+  const { doctors } = useSelector((state) => state.doctors);
 
-  const [pages,setPages] = useState(1);
-  const [page,setPage] = useState(1);
-  const [isSuccess,setIsSuccess] = useState(false)
-  
-    const dispatch = useDispatch();
-    const {doctors} = useSelector((state)=>state.doctors);
-  
-  
-    const onChange = (current,value)=>{
-      
-      setPage(value);
-     
-    }
-  
-  
+  const onChange = (current, value) => {
+    setPage(value);
+  };
 
-    useEffect(()=>{
-      const getAllDoctors = async()=>{
-        try {
-          dispatch(showLoading());
-          const response = await axios.get(
-            `/api/admin/allDoctors?page=${page}`,
-            {
-              headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
-              },
-            }
-          );
-          dispatch(hideLoading());
-          if (response && response.data.success) {
-            setPages(response.data.pages)
-            return dispatch(setDoctors(response.data.doctors));
-          }
-        } catch (error) {
-          dispatch(hideLoading());
-          return toast.error(
-            error.response.data.msg ? error.response.data.msg : error.message,
-            {
-              duration: 1000,
-            }
-          );
+  useEffect(() => {
+    const getAllDoctors = async () => {
+      try {
+        dispatch(showLoading());
+        const response = await axios.get(`/api/admin/allDoctors?page=${page}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        dispatch(hideLoading());
+        if (response && response.data.success) {
+          setPages(response.data.pages);
+          return dispatch(setDoctors(response.data.doctors));
         }
+      } catch (error) {
+        dispatch(hideLoading());
+        return toast.error(
+          error.response.data.msg ? error.response.data.msg : error.message,
+          {
+            duration: 1000,
+          }
+        );
       }
-      
-     getAllDoctors()
-    
-  
-    
-    },[dispatch,page,isSuccess])
+    };
 
+    getAllDoctors();
+  }, [dispatch, page, isSuccess]);
 
-
-
-
-
-const handleStatus = async(doctorId,userId,status)=>{
-  try {
-    dispatch(showLoading());
-    const response = await axios.post('/api/admin/approve-doctor',{doctorId:doctorId,userId:userId,status:status},{
-      headers:{
-        Authorization: `Bearer ${localStorage.getItem('token')}`
+  const handleStatus = async (doctorId, userId, status) => {
+    try {
+      dispatch(showLoading());
+      const response = await axios.post(
+        "/api/admin/approve-doctor",
+        { doctorId: doctorId, userId: userId, status: status },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (response && response.data.success) {
+        toast.success(response.data.msg);
+        setIsSuccess(!isSuccess);
       }
-    });
-    dispatch(hideLoading());
-    if (response && response.data.success) {
-      toast.success(response.data.msg);
-     setIsSuccess(!isSuccess);
-     
+    } catch (error) {
+      dispatch(hideLoading());
+      return toast.error(
+        error.response.data.msg ? error.response.data.msg : error.message,
+        {
+          duration: 1000,
+        }
+      );
     }
-   
-  } catch (error) {
-    dispatch(hideLoading());
-          return toast.error(
-            error.response.data.msg ? error.response.data.msg : error.message,
-            {
-              duration: 1000,
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text, record) => (
+        <span className="normal_text">
+          {record.firstName} {record.lastName}
+        </span>
+      ),
+    },
+    {
+      title: "Specialization",
+      dataIndex: "specialization",
+    },
+    {
+      title: "Phone",
+      dataIndex: "mobileNumber",
+    },
+    {
+      title: "Created At",
+      dataIndex: "createdAt",
+      render: (text, record) => moment(record.createdAt).format("DD-MM-YYYY"),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Action",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          <h1
+            className="anchor"
+            onClick={() =>
+              handleStatus(record._id, record.userId, record.status)
             }
-          );
-  }
-}
-
-
-
-
-
-    const columns = [
-      {
-        title:'Name',
-        dataIndex: 'name',
-        render:(text,record)=> <span className='normal_text'>{record.firstName} {record.lastName}</span>
-      },
-      {
-        title:'Specialization',
-        dataIndex: 'specialization'
-      },
-      {
-        title:'Phone',
-        dataIndex:'mobileNumber'
-      },
-      {
-        title: 'Created At',
-        dataIndex: 'createdAt'
-      },
-      {
-title:'Status',
-dataIndex:'status'
-      },
-      {
-        title:'Action',
-        dataIndex:'actions',
-        render: (text,record)=>(
-          <div className='d-flex'>
-            <h1 className='anchor' onClick={()=>handleStatus(record._id,record.userId,record.status)}>{record.status === 'pending' ? 'Approve' : record.status === 'approved' ? 'Block' : 'Approve'}</h1>
-          
-          </div>
-        )
-    
-        
-      }
-    ]
-
+          >
+            {record.status === "pending"
+              ? "Approve"
+              : record.status === "approved"
+              ? "Block"
+              : "Approve"}
+          </h1>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <>
-    <Layout>
-       <div className='inputContainer'>
-        <h1 className='page-title'>All Doctors:</h1>
-            <input type="text" placeholder='Search Doctors Here'/>
+      <Layout>
+        <div className="inputContainer">
+          <h1 className="page-title">All Doctors:</h1>
+          <input type="text" placeholder="Search Doctors Here" />
         </div>
 
-
-<Table columns={columns} dataSource={doctors}/>
-
-
-    </Layout>
-    {pages > 1 && (
-      <div className="pagination_container">
-    <Stack spacing={2}>
-      <Pagination count={pages} page={page}  onChange={onChange}/>
-      </Stack>
-  </div>
-    )}
+        <Table columns={columns} dataSource={doctors} />
+      </Layout>
+      {pages > 1 && (
+        <div className="pagination_container">
+          <Stack spacing={2}>
+            <Pagination count={pages} page={page} onChange={onChange} />
+          </Stack>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Doctors
+export default Doctors;
--- client/src/Pages/Admin/Users.jsx
+++ client/src/Pages/Admin/Users.jsx
@@ -1,115 +1,98 @@
-import React,{useEffect,useState} from 'react';
-import Layout from '../../components/Layout';
-import { setUsers } from '../../Redux/allUsersSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
+import { setUsers } from "../../Redux/allUsersSlice";
+import { useDispatch, useSelector } from "react-redux";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import axios from "axios";
 import toast from "react-hot-toast";
-import {showLoading,hideLoading} from '../../Redux/alertsSlice'
-import { Table } from 'antd';
-
-
+import { showLoading, hideLoading } from "../../Redux/alertsSlice";
+import { Table } from "antd";
+import moment from "moment";
 
 const Users = () => {
-
-const [pages,setPages] = useState(1);
-const [page,setPage] = useState(1)
+  const [pages, setPages] = useState(1);
+  const [page, setPage] = useState(1);
 
   const dispatch = useDispatch();
-  const {users} = useSelector((state)=>state.users);
+  const { users } = useSelector((state) => state.users);
 
-
-  const onChange = (current,value)=>{
-    
+  const onChange = (current, value) => {
     setPage(value);
-   
-  }
+  };
 
-
-
-
-useEffect(()=>{
-  const getAllUsers = async()=>{
-    try {
-      dispatch(showLoading());
-      const response = await axios.get(
-        `/api/admin/allUsers?page=${page}`,
-        {
+  useEffect(() => {
+    const getAllUsers = async () => {
+      try {
+        dispatch(showLoading());
+        const response = await axios.get(`/api/admin/allUsers?page=${page}`, {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
+        });
+        dispatch(hideLoading());
+        if (response && response.data.success) {
+          setPages(response.data.pages);
+          return dispatch(setUsers(response.data.users));
         }
-      );
-      dispatch(hideLoading());
-      if (response && response.data.success) {
-        setPages(response.data.pages)
-        return dispatch(setUsers(response.data.users));
+      } catch (error) {
+        dispatch(hideLoading());
+        return toast.error(
+          error.response.data.msg ? error.response.data.msg : error.message,
+          {
+            duration: 1000,
+          }
+        );
       }
-    } catch (error) {
-      dispatch(hideLoading());
-      return toast.error(
-        error.response.data.msg ? error.response.data.msg : error.message,
-        {
-          duration: 1000,
-        }
-      );
-    }
-  }
-  
-  getAllUsers()
+    };
 
+    getAllUsers();
+  }, [dispatch, page]);
 
-
-},[dispatch,page])
-
-const columns = [
-  {
-    title:'Name',
-    dataIndex: 'name'
-  },
-  {
-    title:'Email',
-    dataIndex: 'email'
-  },{
-    title: 'Created At',
-    dataIndex: 'createdAt'
-  },
-  {
-    title:'Action',
-    dataIndex:'actions',
-    render: (text,record)=>(
-      <div className='d-flex'>
-        <h1 className='anchor'>Block</h1>
-      </div>
-    )
-
-    
-  }
-]
-
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Created At",
+      dataIndex: "createdAt",
+      render: (text, record) => moment(record.createdAt).format("DD-MM-YYYY"),
+    },
+    {
+      title: "Action",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          <h1 className="anchor">Block</h1>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <>
-    <Layout>
-       
-        <div className='inputContainer'>
-        <h1 className='page-title'>All Users:</h1>
-            <input type="text" placeholder='Search Users Here'/>
+      <Layout>
+        <div className="inputContainer">
+          <h1 className="page-title">All Users:</h1>
+          <input type="text" placeholder="Search Users Here" />
         </div>
 
-<Table columns={columns} dataSource={users}/>        
-    </Layout>
-    {pages > 1 && (
-      <div className="pagination_container">
-    <Stack spacing={2}>
-      <Pagination count={pages} page={page}  onChange={onChange}/>
-      </Stack>
-  </div>
-    )}
-    
-  </>
-  )
-}
+        <Table columns={columns} dataSource={users} />
+      </Layout>
+      {pages > 1 && (
+        <div className="pagination_container">
+          <Stack spacing={2}>
+            <Pagination count={pages} page={page} onChange={onChange} />
+          </Stack>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Users
+export default Users;
